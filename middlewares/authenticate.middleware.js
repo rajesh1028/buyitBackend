4abd12f6580d3fce34859cbf4bbe6fc60b6b70d6
rajesh-fw22-0030 <rajeshkaruppusamy@gmail.com>
2023-07-20 12:38:55 +0530

    cart route setup and deployed

--- middlewares/authenticate.middleware.js
+++ middlewares/authenticate.middleware.js
@@ -4,14 +4,17 @@ require("dotenv").config()
 const authenticate = (req, res, next) => {
     const token = req.headers.authorization
     if (token) {
-        const decoded = jwt.verify(token, process.env.key)
-        if (decoded) {
-            const userID = decoded.userID;
-            req.body.userID = userID;
-            next();
-        } else {
-            res.status(401).json({ "msg": "Please login first" });
-        }
+        jwt.verify(token, process.env.key, (err, decoded) => {
+            if (err) {
+                console.error('JWT verification failed:', err.message);
+                res.status(401).json({ "msg": err.message });
+            } else {
+                console.log('JWT verified successfully!');
+                const userID = decoded.userID;
+                req.body.userID = userID;
+                next();
+            }
+        });
     } else {
         res.status(401).json({ "msg": "Please login first" });
     }
